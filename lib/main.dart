352d5c0dfb310865e15b9aed14c9622051ac12d8
mo7a3dil <3moha3dil@gmail.com>
2026-04-03 20:46:42 +0300

    adding the connection for database

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FLKFJASDLKFJKLASDFJLKSDAFJKLDSJF',
+      title: 'wsfm',
       theme: ThemeData(
         // This is the theme of your application.
         //
